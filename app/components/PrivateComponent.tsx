8da9d852754a9875c449ca627afb9b5cb2ae2533
Jehú <jehu.sagardoy@gmail.com>
2022-11-25 17:34:40 +0100

    Fixes component behaviour to redirect to login if not logged

--- app/components/PrivateComponent.tsx
+++ app/components/PrivateComponent.tsx
@@ -1,19 +1,37 @@
 'use client'
 
+import React, { useEffect } from 'react'
 import { Session, useSession, useUser } from '@supabase/auth-helpers-react'
 
 import AccessDenied from './AccessDenied'
-import React from 'react'
+import { AlertType } from '../../utils/types'
+import { useRouter } from 'next/navigation'
+import useSnackbar from '../hooks/useSnackbar'
 
 interface Props {
   children: JSX.Element
 }
+
 const PrivateComponent = ({ children }: Props) => {
+  const router = useRouter()
+  const { setAlert } = useSnackbar()
   const session: Session | null = useSession()
+
+  useEffect(() => {
+    const newAlert: AlertType = {
+      open: true,
+      severity: 'error',
+      message:
+        'Insuficient permission. You need to be logged to access to this page.',
+    }
+    if (!session) {
+      setAlert(newAlert)
+      router.push('/login')
+    }
+  }, [])
+
   if (session) {
     return children
-  } else {
-    return <AccessDenied />
   }
 }
 
