a7caa57ff692febd1caafd0b51d4715eacb32b7a
Jehú <jehu.sagardoy@gmail.com>
2022-12-23 10:47:00 +0100

    added database types

---
+++ database/database.types.ts
@@ -0,0 +1,53 @@
+export type Profile = {
+    userId: string,
+    username: string,
+    fullName: string,
+    avatarURL: string,
+    website: string,
+    vken: string,
+    rol: string
+}
+
+export type PlayersInTable = {
+    id: string,
+    playerId: string,
+    tournamentId: string,
+    VP: number,
+    GW: number,
+    minipoints: number,
+    coinflip: number,
+    round: number,
+    tableRank: number,
+    dropped: boolean,
+}
+
+export type Player = {
+    id: string,
+    tournamentId: string,
+    ranking: number,
+    userId: string,
+}
+
+export type Table = {
+    id: string,
+    round: number,
+    tournamentId:string,
+}
+export type Tournament = {
+    tournamentId: string,
+    name: string,
+    description: string,
+    date: Date,
+    hour: string,
+    numberOfRounds: number,
+    maxNumberOfPlayers: number,
+    active: boolean,
+    details: string,
+    price: string,
+    country: string,
+    state: string,
+    city: string,
+    address: string,
+    currency: string
+    userId: string,
+}
