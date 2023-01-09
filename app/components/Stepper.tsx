c2669b7b8a9bd35e5295f00952c09a4ea1ea5fb4
Jehú <jehu.sagardoy@gmail.com>
2023-01-09 13:56:22 +0100

    multistep component

---
+++ app/components/Stepper.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import { Box, Button, Container } from '@mui/material'
+import React, { ReactElement } from 'react'
+
+import useMultistep from '../hooks/useMultistep'
+
+const Stepper = ({ steps }: { steps: ReactElement[] }) => {
+  const { currentStepIndex, step, isFirstStep, isLastStep, back, next } =
+      useMultistep(steps)
+    const handleNext = () => {
+        if (!isLastStep) { return next() }
+        alert('Done')
+    }
+  return (
+    <Container>
+      <Box>
+        {currentStepIndex + 1}/{steps.length}
+      </Box>
+      <Box>{step}</Box>
+      <Box>
+        {!isFirstStep() && <Button onClick={back}>Back</Button>}
+        <Button onClick={()=>handleNext}>{isLastStep() ? 'Finish' : 'Next'}</Button>
+      </Box>
+    </Container>
+  )
+}
+
+export default Stepper
---
+++ app/hooks/useMultistep.tsx
@@ -0,0 +1,37 @@
+import { ReactElement, useState } from 'react'
+const useMultistep = (steps: ReactElement[]) => {
+  const [currentStepIndex, setCurrentStepIndex] = useState(0)
+
+  const next = () => {
+    setCurrentStepIndex((prev) => {
+      if (prev >= steps.length) {
+        return prev
+      }
+      return prev + 1
+    })
+  }
+  const back = () => {
+    setCurrentStepIndex((prev) => {
+      if (prev <= 0) {
+        return prev
+      }
+      return prev - 1
+    })
+  }
+  const goto = (index: number) => {
+    setCurrentStepIndex(index)
+  }
+  const isFirstStep = () => currentStepIndex === 0
+
+  const isLastStep = () => currentStepIndex === steps.length - 1
+  return {
+    currentStepIndex,
+    step: steps[currentStepIndex],
+    isFirstStep,
+    isLastStep,
+    next,
+    back,
+    goto,
+  }
+}
+export default useMultistep
